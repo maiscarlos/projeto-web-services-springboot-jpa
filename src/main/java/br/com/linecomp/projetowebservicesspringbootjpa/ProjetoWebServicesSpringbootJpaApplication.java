3dcecee96246f224d477c97ea028168fc27866a0
dev Carlos <maiscarlos@gmail.com>
2022-01-27 15:05:00 -0300

    Project created

---
+++ src/main/java/br/com/linecomp/projetowebservicesspringbootjpa/ProjetoWebServicesSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package br.com.linecomp.projetowebservicesspringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoWebServicesSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoWebServicesSpringbootJpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/linecomp/projetowebservicesspringbootjpa/ProjetoWebServicesSpringbootJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.linecomp.projetowebservicesspringbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoWebServicesSpringbootJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
